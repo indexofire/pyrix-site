f3396048e7baef10c41574b08981b1686cc40410
Mark Renton <indexofire@gmail.com>
2010-08-18 16:57:30 +0800

    update themes

---
+++ pyrix_site/profile/__init__.py
@@ -0,0 +1,8 @@
+"""
+Introduction:
+Profile app is revised from django-basic-apps. The app repository is at here:
+http://github.com/nathanborror/django-basic-apps/tree/master/basic/profiles/
+
+Usage:
+
+"""
