62dc18732f068fd59fe81e1c8fe9ee3fab19f366
Mark Renton <indexofire@gmail.com>
2010-08-24 16:20:07 +0800

    fix typo

--- conf/django_wsgi.py
+++ conf/django_wsgi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys
 import django.core.handlers.wsgi
-sys.path.append(‘/home/mark/www/’)
-os.environ['DJANGO_SETTINGS_MODULE'] = ‘settings’
+sys.path.append('/home/mark/www/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 application = django.core.handlers.wsgi.WSGIHandler()
