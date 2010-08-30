caf2d076b08ccee3851a3c4b18e836d93ebe7e3f
Mark Renton <indexofire@gmail.com>
2010-08-30 16:34:10 +0800

    update

--- pyrix_site/account/urls.py
+++ pyrix_site/account/urls.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 from django.contrib.auth.decorators import login_required
-import views
+
+from account.views import *
+
 
 urlpatterns = patterns('',
     url(r'^$', login_required(views.profile), name='account_index'),
-    url(r'^signature/$', views.signature, name='signature'),
+    url(r'^signature/$', signature, name='signature'),
     (r'^', include('registration.backends.default.urls')),
 )
