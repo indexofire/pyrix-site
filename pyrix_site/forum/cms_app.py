89ab933f05d410c7c7d3eb1403ae838a6ad5aa48
Mark Renron <indexofire@gmail.com>
2010-09-09 19:26:25 +0800

    remove forum menu

--- pyrix_site/forum/cms_app.py
+++ pyrix_site/forum/cms_app.py
@@ -3,12 +3,12 @@ from django.utils.translation import ugettext_lazy as _
 
 from cms.app_base import CMSApp
 from cms.apphook_pool import apphook_pool
-from forum.menu import ForumMenu
+#from forum.menu import ForumMenu
 
 
 class ForumApp(CMSApp):
     name = _(u"Disscusion Board")
     urls = ["forum.urls"]
-    menus = [ForumMenu]
+    #menus = [ForumMenu]
     
 apphook_pool.register(ForumApp)
